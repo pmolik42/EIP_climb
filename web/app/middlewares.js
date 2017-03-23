e67de54988faf0cb3e4d669df6048b53cdde3b0c
Younes <younes.iddahamou-idrissi@epitech.eu>
2017-03-23 21:44:09 +0800

    es5 to es6 but without imports, express and project tree updated

---
+++ web/app/middlewares.js
@@ -0,0 +1,57 @@
+
+
+// route middleware to make sure the user is logged
+const isLoggedIn = (req, res, next) => {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+};
+
+// route middleware to make sure the token is valid
+const isTokenValid = (req, res, next) => {
+
+  // check header or url parameters or post parameters for token
+  let token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+
+        let user = {
+          id: decoded._doc._id,
+          email: decoded._doc.local.email,
+          username: decoded._doc.profile.username,
+          createdAt: decoded._doc.createdAt
+        };
+
+        // if everything is good, save to request for use in other routes
+        req.user = user;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+
+  }
+};
+
+	module.exports = {
+	  isLoggedIn : isLoggedIn,
+		isTokenValid : isTokenValid
+	};
--- web/app/routes.js
+++ web/app/routes.js
@@ -1,77 +1,9 @@
-var signup = require('./modules/auth/signup.js');
+const authRoutes = require('./modules/auth/routes.js');
+const homeRoutes = require('./modules/home/routes.js');
 
-// app/routes.js
-module.exports = function(app, passport) {
-
-	app.get('/', function(req, res) {
-		res.render('index.ejs', { data: req.session.data });
-		delete req.session.data;
-	});
-
-	// process the login form
-	app.post('/login', function(req, res) {
-		passport.authenticate('local-login', function(error, user, data) {
-    	if (error || !user) {
-				req.session.data = data;
-				return res.redirect('/');
-			} else {
-				req.login(user, function(err) {
-					if (err)
-						throw err;
-					return res.redirect('/profile');
-				});
-			}
-  	})(req, res);
-	});
-
-	// process the signup form
-	app.post('/signup', function(req, res) {
-
-		var userInfos = {
-			username: req.param('username') || '',
-			email: req.param('email') || '',
-			password: req.param('password'),
-			confirmPassword: req.param('confirmPassword')
-		};
-
-		signup(userInfos, false, function(error, user, data) {
-			if (error || !user) {
-				req.session.data = data;
-				return res.redirect('/#toregister');
-			} else {
-				req.login(user, function(err) {
-					if (err)
-						throw err;
-					return res.redirect('/profile');
-				});
-			}
-		});
-
-	});
-
-	// PROFILE SECTION
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
-	// LOGOUT
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+const configRoutes = (app, passport) => {
+	authRoutes(app, passport);
+	homeRoutes(app);
 };
 
-// route middleware to make sure
-function isLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
+module.exports = configRoutes;
