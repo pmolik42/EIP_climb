336013d85fb6250df26fd69546d879b501259414
Younes <younes.iddahamou-idrissi@epitech.eu>
2017-09-12 17:57:41 +0200

    change gitignore

---
+++ android/app/src/main/java/com/climb/eip/climb/activities/MediaPlayerActivity.java
@@ -0,0 +1,10 @@
+package com.climb.eip.climb.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Younes on 11/09/2017.
+ */
+
+public class MediaPlayerActivity extends AppCompatActivity {
+}
